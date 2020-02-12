82210fee4b3a99a8a7156ecd46a9c9c51265a90d
sandiwara <davidazhari73@gmail.com>
2020-02-12 23:08:09 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# optical-character-recognition-clientSide-processing
-Optical Character Recognition - Gambaran kecil salah satu metode yang mungkin dapat diterapkan dalam meminimalisir "Human error" atau pun "Human Manipulation" yang dipersengketakan dalam PEMILU 2019
+
+<img src="screenshot.png" height="auto" style="width:100%;  margin:0 
+
+ 
+## Prakata
+
+Isu kecurangan PEMILU 2019 seolah mendapati pijakan kakinya dibumi, menyelinap dalam benak, lalu merasuki sanubari melalui siratan bukti-bukti yang tak dapat begitu saja di tampik oleh semua pihak yang terlibat, ataupun mereka-mereka yang memiliki kepentingan dalam pelaksanaan PEMILU kali ini.
+
+Terdapat satu pertanyaan yang di ajukan oleh hakim ketua dalam sidang sengketa kecurangan pemilu 2019 yang kemudian mendasari adanya repository ini. Adapun maksudnya adalah untuk memberi gambaran bahwa "human error" atau kesalahan-kesalahan teknis akibat kelalaian yang di timbulkan oleh manusia sesungguhnya dapat diminimalisir bahkan dapat dicegah jika terdapat sebuah komitment etik dari para pengampuh kekuasaan dalam setiap pangambilan kebijakan.
+
+Rakyat mungkin dapat dikelabui / dikaburkan nalar dengan sengkarut anrgumentasi retoris nan apologetik, namun segala bentuk pretensi politis tersebut tentu tak dapat dengan serta-merta membatah jika 1 + 1 = 2. Logika dasar itulah yang kasat mencuat kepermukaan kala data hasil PEMILU yang dimuat dalam situng KPU (Komisi Pemilihan Umum) tak serima dalam nada-nada ekuivalen sebagaimana maktubah legal formil C1-nya.
+
+Terlepas dari kelalaian atupun kesengajaan, sesungguhnya hal tersebut adalah secuil bagian dari penampakan atas pola "penindasan" di negeri kita yang bercorak "Setengah Jajahan (Kapitalisme) - Setengah Feodal". Kapitalisme membawa teknologi berkembang pesat dalam rangka efisiensi guna mengakumulasikan kapital, sedangkan bonggol feodalisme masih belum tercerabut secara penuh, berupaya mempertahankan diri dalam segan hidupnya karena mati pun ia tak mau.
+
+Dalam realitasnya, dua unsur yang seolah bertolak belakang tersebut senyatanya saling berkompromi, sejalan dalam kerangka penghisapan. Kontradiksi antara pembodohan dan tuntutan  logis algoritmik yang di bawah oleh masing-masing "Tuan" itulah sedang yang kita lihat sebagai batu sandungan yang entah akan menjungkalkan salah satu atau keduannya? atau mungkin malah batu itulah yang malah akan digunakan untuk menimpuk kita semua.
+
+## Pengertian
+[Optical Character Recognition / OCR ](https://id.wikipedia.org/wiki/Pengenalan_karakter_optis) *(Pengenalan Karakter Optik)*  adalah sebuah perangkat lunak yang mengubah teks dalam format berkas citra atau gambar ke dalam format teks yang bisa dibaca dan disunting oleh aplikasi komputer [1]. Berkas teks berformat citra tersebut didapatkan dengan cara memindai atau memfoto sebuah buku, manuskrip, tulisan di papan pengumuman, ataupun materi kuliah di papan tulis dsb. Sedangkan alat yang diunakan untuk memindai adalah pemindai *(scanner)* atau kamera baik kamera DSLR ataupun kamera di ponsel pintar.
+
+Sebagai perangkat lunak yang kompleks, OCR diterapkan di berbagai bidang seperti untuk membaca data secara otomatis dan langsung memasukkannya ke dalam basis data -- contohnya seperti dalam pemindaian passport, nota transfer bank, kertas berharga – untuk pengenalan pelat kendaraan bermotor dari video atau gambar yang tertangkap kamera, dan yang paling marak adalah untuk preservasi konten buku-buku sumber referensi utama dan manuskrip bersejarah. Dalam beberapa kasus, OCR berkontribusi sangat besar dalam proses pembangunan perpustakaan digital.
+
+
+    
+## Penggunaan
+1. Unduh Repository ini.
+2. Buka menggunakan "teks editor".
+3. jalankan dengan "live-server".
+    
+## Library
+ - [OPTICAL CARACTER RECOGNITION (OCR)](https://github.com/naptha/tesseract.js)  
+  
+[Optical Character Recognition / OCR ](https://id.wikipedia.org/wiki/Pengenalan_karakter_optis) *(Pengenalan Karakter Optik)*  adalah sebuah perangkat lunak yang mengubah teks dalam format berkas citra atau gambar ke dalam format teks yang bisa dibaca dan disunting oleh aplikasi komputer [1]. Berkas teks berformat citra tersebut didapatkan dengan cara memindai atau memfoto sebuah buku, manuskrip, tulisan di papan pengumuman, ataupun materi kuliah di papan tulis dsb. Sedangkan alat yang diunakan untuk memindai adalah pemindai *(scanner)* atau kamera baik kamera DSLR ataupun kamera di ponsel pintar.
+
+Sebagai perangkat lunak yang kompleks, OCR diterapkan di berbagai bidang seperti untuk membaca data secara otomatis dan langsung memasukkannya ke dalam basis data -- contohnya seperti dalam pemindaian passport, nota transfer bank, kertas berharga – untuk pengenalan pelat kendaraan bermotor dari video atau gambar yang tertangkap kamera, dan yang paling marak adalah untuk preservasi konten buku-buku sumber referensi utama dan manuskrip bersejarah. Dalam beberapa kasus, OCR berkontribusi sangat besar dalam proses pembangunan perpustakaan digital.
+
+
+- OPTICAL CARACTER RECOGNITION (OCR)
+https://github.com/naptha/tesseract.js
---
+++ assets/js/script.js
@@ -0,0 +1,232 @@
+const sampleBtn = document.querySelector('.sample'),
+    processContainer = document.querySelector('.process-container'),
+    prevContainer = document.querySelector('.img-preview'),
+    runBtn = document.querySelector('.run'),
+    imgPrev = document.querySelector('.img-preview img'),
+    svgIcon = document.querySelector('.svg-icon'),
+    processBtn = document.querySelector('.process-this'),
+    uploadBtn = document.querySelector('.upload'),
+    resultText = document.querySelector('#result-text'),
+    copyBtn = document.querySelector('.copy-btn'),
+    tooltip = document.getElementById("myTooltip"),
+    process = document.querySelector('#process');
+
+// resultText.addEventListener('mouseenter', function () {
+//     // console.log('enter');
+//     copyBtn.style = "opacity: 1"
+
+// }, false);
+
+// resultText.addEventListener('mouseleave', function () {
+//     // console.log('leave');
+//     copyBtn.style.opacity = "";
+
+// }, false)
+
+
+copyBtn.addEventListener('click', function () {
+    let preText = resultText.querySelector('pre'),
+        range = document.createRange(),
+        selection = window.getSelection();
+
+    range.selectNodeContents(preText);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+
+
+    // update tooltip text
+    tooltip.innerHTML = "Copied";
+
+    console.log(range.toString());
+
+})
+
+sampleBtn.addEventListener('click', function () {
+    console.log('sample');
+    // console.log(imgPrev.src);
+    // console.log(this);
+
+    imgPrev.src = 'puisi.jpg';
+    imgPrev.style = 'display :block;';
+    svgIcon.style = 'display :none';
+    process.style = 'display :none';
+    runBtn.style = 'display :inline-block';
+
+    prevContainer.querySelector('.overlay').style = "display :none";
+
+    // console.log(this.parentElement.parentElement.parentElement);
+    process.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+    });
+
+})
+
+prevContainer.addEventListener('mouseenter', function (e) {
+    // console.log(e);
+    // console.log(this.querySelector('.overlay').clientHeight);
+    // console.log(this.querySelector('img').clientHeight);
+
+    if (imgPrev.currentSrc == '') {
+        // console.log("null mase");
+        processBtn.style = 'display :none';
+        sampleBtn.style = 'display :block';
+
+    } else {
+
+        processBtn.style = 'display :none';
+        sampleBtn.style = 'display :none';
+        this.querySelector('.overlay').style = 'display :none';
+        // this.querySelector('.overlay').style = `height: ${this.querySelector('img').clientHeight}px; width: ${this.querySelector('img').clientWidth}px; `;
+    }
+    // console.log(imgPrev.currentSrc);
+
+
+});
+runBtn.addEventListener('click', function () {
+    processBtn.click()
+})
+
+processBtn.addEventListener('click', function () {
+    imgPrev.style = 'width:auto;  height: 500px; display :block;';
+    process.style = 'display :block';
+    prevContainer.style = 'width :30%';
+
+    this.style = 'display :none';
+
+    // prevContainer.querySelector('.overlay').style = `height: ${prevContainer.querySelector('img').clientHeight}px; width: ${prevContainer.querySelector('img').clientWidth}px`;
+
+
+
+    recognizeFile(imgPrev.src);
+});
+
+uploadBtn.addEventListener('change', function (params) {
+    const file = params.target.files[0];
+    // console.log(params.target.files.length);
+    this.querySelector('label').textContent = file.name;
+
+    const reader = new FileReader();
+
+    if (params.target.files.length > 0) {
+        // prevContainer.style = 'width :100%';
+
+        // console.log(reader);
+        reader.readAsDataURL(file)
+        reader.onload = function (re) {
+            // console.log(re.target.result);
+            imgPrev.src = re.target.result;
+            imgPrev.style = 'width:100%; display :block';
+            processBtn.style = 'display :none';
+            process.style = 'display :none';
+            prevContainer.style = 'width :100%';
+
+            svgIcon.style = 'display :none';
+
+            runBtn.style = 'display :inline-block';
+
+            // prevContainer.querySelector('.overlay').style = `height: ${prevContainer.querySelector('img').clientHeight}px; width: ${prevContainer.querySelector('img').clientWidth}`;
+        }
+    }
+});
+
+
+/* ==============================================
+* Tesseract Config
+============================================== */
+
+const winOrigin = window.origin;
+console.log(window.origin);
+// setting agar load dari localhost
+window.Tesseract = Tesseract.create({
+    // Path to worker
+    workerPath: `${winOrigin}/worker.js`,
+    // Path of folder where the language trained data is located
+    langPath: `${winOrigin}/langsdata/`,
+    // Path to index script of the tesseract core ! https://github.com/naptha/tesseract.js-core
+    // corePath: "http://127.0.0.1:5500/index.js",
+    corePath: `${winOrigin}/index.js`,
+});
+
+
+
+function recognizeFile(file) {
+    process.innerHTML = '';
+    process.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+    });
+    Tesseract.recognize(file, document.querySelector('#langsel').value)
+        .progress(function (packet) {
+            // console.info(packet)
+            progressUpdate(packet)
+
+        })
+        .then(function (data) {
+            // console.log(data)
+            progressUpdate({
+                status: 'done',
+                data: data
+            })
+        })
+};
+
+
+function progressUpdate(packet) {
+    // var log = document.getElementById('log');
+    const log = document.getElementById('process');
+    const result = document.getElementById('result-text'),
+        theText = result.querySelector('.the-text');
+
+    if (log.firstChild && log.firstChild.status === packet.status) {
+
+        if ('progress' in packet) {
+            var progress = log.firstChild.querySelector('progress')
+            progress.value = packet.progress
+        }
+
+
+    } else {
+        var line = document.createElement('div');
+        line.status = packet.status;
+
+        var status = document.createElement('div')
+        status.className = 'status'
+
+        status.appendChild(document.createTextNode(packet.status))
+        line.appendChild(status)
+
+
+
+        if ('progress' in packet) {
+            var progress = document.createElement('progress')
+            progress.value = packet.progress
+            progress.max = 1
+            line.appendChild(progress)
+        }
+
+
+        if (packet.status == 'done') {
+            // jika Telah selesai
+            var pre = document.createElement('pre')
+            pre.appendChild(document.createTextNode(packet.data.text))
+            // line.innerHTML = ''
+            // line.appendChild(pre)
+            result.style = "display: block"
+            theText.innerHTML = ''
+            theText.appendChild(pre)
+
+            result.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center'
+            });
+        }
+
+        log.insertBefore(line, log.firstChild)
+        // log.insertAfter(line, log.lastChild)
+    }
+}
